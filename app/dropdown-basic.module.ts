c394ba5c376bf48d82f6b51ae7b94a32104d9a00
arashcy <arashcyruss@gmail.com>
2023-03-03 09:01:29 -0600

    4 files modified

--- app/dropdown-basic.module.ts
+++ app/dropdown-basic.module.ts
@@ -4,9 +4,6 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { A11yModule } from '@angular/cdk/a11y';
 import { NgbdDropdownBasic } from './dropdown-basic';
-import { GuidesAndFaqsComponent } from './guides-and-faqs/guides-and-faqs.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FeedbackComponent } from './feedback/feedback.component';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
@@ -16,19 +13,10 @@ import { ListComponent } from './list/list.component';
     A11yModule,
     RouterModule.forRoot([
       { path: 'list', component: ListComponent },
-      { path: 'guide-faq', component: GuidesAndFaqsComponent },
-      { path: 'contact-us', component: ContactUsComponent },
-      { path: 'feedback', component: FeedbackComponent },
       { path: '**', redirectTo: '/list', pathMatch: 'full' },
     ]),
   ],
-  declarations: [
-    NgbdDropdownBasic,
-    GuidesAndFaqsComponent,
-    ContactUsComponent,
-    FeedbackComponent,
-    ListComponent,
-  ],
+  declarations: [NgbdDropdownBasic, ListComponent],
   exports: [NgbdDropdownBasic],
   bootstrap: [NgbdDropdownBasic],
 })
--- app/dropdown-basic.ts
+++ app/dropdown-basic.ts
@@ -1,8 +1,30 @@
-import {Component} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ngbd-dropdown-basic',
-  templateUrl: './dropdown-basic.html'
+  templateUrl: './dropdown-basic.html',
 })
 export class NgbdDropdownBasic {
+  @ViewChild('toc', { static: false }) toc: ElementRef;
+  @ViewChild('closeBtn', { static: false }) closeBtn: ElementRef;
+  @ViewChild('menu', { static: false }) menu: ElementRef;
+
+  constructor() {
+    window.addEventListener('keydown', (e) => {
+      console.log(e);
+    });
+  }
+
+  open() {
+    try {
+      let keyboardEvent = new KeyboardEvent('keydown', {
+        key: 'Tab',
+        bubbles: true,
+      });
+      // console.log(this.toc);
+      this.menu.nativeElement.dispatchEvent(keyboardEvent);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
